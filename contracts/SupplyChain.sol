b16c33ab7606da11f6b2d840cdc411f4c8beb3d5
cds-amal <cds.sudama@gmail.com>
2021-09-09 11:27:16 -0400

    really solved

--- contracts/SupplyChain.sol
+++ contracts/SupplyChain.sol
@@ -18,30 +18,10 @@ contract SupplyChain {
     address payable buyer;
   }
   
-  /* 
-   * Events
-   */
-
-  // <LogForSale event: sku arg>
   event LogForSale(uint256 sku);
-
-  // <LogSold event: sku arg>
   event LogSold(uint256 sku);
-
-  // <LogShipped event: sku arg>
   event LogShipped(uint256 sku);
-
-  // <LogReceived event: sku arg>
   event LogReceived(uint256 sku);
-
-
-  /* 
-   * Modifiers
-   */
-
-  // Create a modifer, `isOwner` that checks if the msg.sender is the owner of the contract
-
-  // <modifier: isOwner
 
   modifier verifyCaller (address _address) { 
     require (msg.sender == _address); 
@@ -61,46 +41,32 @@ contract SupplyChain {
     items[_sku].buyer.transfer(amountToRefund);
   }
 
-  // For each of the following modifiers, use what you learned about modifiers
-  // to give them functionality. For example, the forSale modifier should
-  // require that the item with the given sku has the state ForSale. Note that
-  // the uninitialized Item.State is 0, which is also the index of the ForSale
-  // value, so checking that Item.State == ForSale is not sufficient to check
-  // that an Item is for sale. Hint: What item properties will be non-zero when
-  // an Item has been added?
-
   modifier forSale(uint _sku) {
     require(items[_sku].state == State.ForSale);
     _;
   }
 
-  // modifier sold(uint _sku) 
   modifier sold(uint _sku) {
     require(items[_sku].state == State.Sold);
     _;
   }
 
-  // modifier shipped(uint _sku) 
   modifier shipped(uint _sku) {
     require(items[_sku].state == State.Shipped);
     _;
   }
 
-  // modifier received(uint _sku) 
   modifier received(uint _sku) {
     require(items[_sku].state == State.Received);
     _;
   }
 
   constructor() public {
-    // 1. Set the owner to the transaction sender
     owner = msg.sender;
-    // 2. Initialize the sku count to 0. Question, is this necessary?
     skuCount = 0;
   }
 
   function addItem(string memory _name, uint _price) public returns (bool) {
-    // 1. Create a new item and put in array
     items[skuCount] = Item({
       name: _name, 
       sku: skuCount, 
@@ -109,28 +75,11 @@ contract SupplyChain {
       seller: msg.sender, 
       buyer: address(0)
     });
-    //
-    // 2. Increment the skuCount by one
     skuCount = skuCount + 1;
-
-    // 3. Emit the appropriate event
     emit LogForSale(skuCount);
-
-    // 4. return true
     return true;
   }
 
-  // Implement this buyItem function. 
-  // 1. it should be payable in order to receive refunds
-  // 2. this should transfer money to the seller, 
-  // 3. set the buyer as the person who called this transaction, 
-  // 4. set the state to Sold. 
-  // 5. this function should use 3 modifiers to check 
-  //    - if the item is for sale, 
-  //    - if the buyer paid enough, 
-  //    - check the value after the function is called to make 
-  //      sure the buyer is refunded any excess ether sent. 
-  // 6. call the event associated with this function!
   function buyItem(uint sku)
   public payable forSale(sku) paidEnough(sku) checkValue(sku)
   {
@@ -140,11 +89,6 @@ contract SupplyChain {
     emit LogSold(sku);
   }
 
-  // 1. Add modifiers to check:
-  //    - the item is sold already 
-  //    - the person calling this function is the seller. 
-  // 2. Change the state of the item to shipped. 
-  // 3. call the event associated with this function!
   function shipItem(uint sku)
   public sold(sku) verifyCaller(items[sku].seller)
   {
@@ -152,11 +96,6 @@ contract SupplyChain {
     emit LogShipped(sku);
   }
 
-  // 1. Add modifiers to check 
-  //    - the item is shipped already 
-  //    - the person calling this function is the buyer. 
-  // 2. Change the state of the item to received. 
-  // 3. Call the event associated with this function!
   function receiveItem(uint sku)
   public shipped(sku) verifyCaller(items[sku].buyer)
   {
@@ -164,7 +103,6 @@ contract SupplyChain {
     emit LogReceived(sku);
   }
 
-  // Uncomment the following code block. it is needed to run tests
   function fetchItem(uint _sku) public view 
     returns (string memory name, uint sku, uint price, uint state, address seller, address buyer)
   {
